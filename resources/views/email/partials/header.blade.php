2aa1d09f481bb8cf91dd89237ad8ca2524c7daab
Matt Stauffer <matt@tighten.co>
2015-05-29 16:23:55 -0400

    Full width columns on mobil

--- resources/views/email/partials/header.blade.php
+++ resources/views/email/partials/header.blade.php
@@ -4,7 +4,7 @@
       <table class="twelve columns">
         <tr>
           <td>
-            <img width="580" height="226" src="https://s3.amazonaws.com/f.cl.ly/items/0Y3X2P2T2N0y0g3w1b2l/tmp.jpg">
+            <img width="580" height="184" src="https://s3.amazonaws.com/f.cl.ly/items/240k3w333s1Y3Z1t1C3X/tmp.jpg">
           </td>
           <td class="expander"></td>
         </tr>
