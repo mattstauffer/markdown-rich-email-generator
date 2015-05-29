b0c380506b58f82f359ad492d566cf1b103768a2
Matt Stauffer <matt@tighten.co>
2015-05-29 08:52:03 -0400

    Tweak design more.

--- resources/views/email/layout.blade.php
+++ resources/views/email/layout.blade.php
@@ -4,6 +4,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width"/>
   <style type="text/css">
+    @import url(http://fonts.googleapis.com/css?family=Lato:400,700);
     <?php include(public_path() . '/css/zurb.css'); ?>
     <?php include(public_path() . '/css/custom.css'); ?>
   </style>
@@ -13,8 +14,6 @@
     <tr>
       <td class="center" align="center" valign="top">
         <center>
-
-
           <table class="row header">
             <tr>
               <td class="center" align="center">
@@ -154,35 +153,9 @@
                         <tr>
                           <td class="left-text-pad">
 
-                            <h5>Connect With Us:</h5>
+                            <p>Email: <a href="mailto:matt@mattstauffer.co">matt@mattstauffer.co</a></p>
 
-                            <table class="tiny-button facebook">
-                              <tr>
-                                <td>
-                                  <a href="#">Facebook</a>
-                                </td>
-                              </tr>
-                            </table>
-
-                            <br>
-
-                            <table class="tiny-button twitter">
-                              <tr>
-                                <td>
-                                  <a href="#">Twitter</a>
-                                </td>
-                              </tr>
-                            </table>
-
-                            <br>
-
-                            <table class="tiny-button google-plus">
-                              <tr>
-                                <td>
-                                  <a href="#">Google +</a>
-                                </td>
-                              </tr>
-                            </table>
+                            <p>Twitter: <a href="http://twitter.com/stauffermatt">@stauffermatt</a></p>
 
                           </td>
                           <td class="expander"></td>
@@ -195,29 +168,9 @@
                       <table class="six columns">
                         <tr>
                           <td class="last right-text-pad">
-                            <h5>Contact Info:</h5>
-                            <p>Phone: 408.341.0600</p>
-                            <p>Email: <a href="mailto:hseldon@trantor.com">hseldon@trantor.com</a></p>
-                          </td>
-                          <td class="expander"></td>
-                        </tr>
-                      </table>
+                            <p>You're receiving this email because you signed up for it at mattstauffer.co</p>
 
-                    </td>
-                  </tr>
-                </table>
-
-
-                <table class="row">
-                  <tr>
-                    <td class="wrapper last">
-
-                      <table class="twelve columns">
-                        <tr>
-                          <td align="center">
-                            <center>
-                              <p style="text-align:center;"><a href="#">Terms</a> | <a href="#">Privacy</a> | <a href="#">Unsubscribe</a></p>
-                            </center>
+                            <p><a href="#">Terms</a> | <a href="#">Privacy</a> | <a href="#">Unsubscribe</a></p>
                           </td>
                           <td class="expander"></td>
                         </tr>
